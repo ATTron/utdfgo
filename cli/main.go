b9d0bf31058e393d5d2f390cfe6505021fe6d475
Anthony Templeton <twantempleton11@gmail.com>
2019-07-06 15:20:16 -0400

    UG-8: update cli functionality

--- cli/main.go
+++ cli/main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os"
+	"reflect"
+
+	"github.com/attron/utdfgo"
 )
 
 func main() {
@@ -11,5 +16,21 @@ func main() {
 	ocall := flag.String("output", "utdfgo", "save output of cli to file")
 	flag.Parse()
 
+	f, err := os.OpenFile(*ocall, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal("Error writing to file:", *ocall)
+	}
+	defer f.Close()
+	utdf := utdfgo.Run(*fname)
+	for _, p := range utdf {
+		cmd := reflect.ValueOf(p).MethodByName(*rcall)
+		output := cmd.Call([]reflect.Value{})
+		line, _ := fmt.Printf("%v\n", output[0])
+		fmt.Println(line)
+		// TODO: handle saving to output file
+		// if _, err := f.WriteString(string(line)); err != nil {
+		// 	log.Fatal("Cannot write line(s) to file", err)
+		// }
+	}
 	fmt.Printf("fname: %s, rcall: %s, ocall: %s", *fname, *rcall, *ocall)
 }
