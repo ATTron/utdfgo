10aacb17cf7ff2beb2ca4f7ea530fcfc3a5d9f88
Anthony Templeton <twantempleton11@gmail.com>
2019-07-07 17:23:12 -0400

    UG-8: add release binaries, update makefile, modify cli functionality

--- Makefile
+++ Makefile
@@ -2,8 +2,9 @@
 
 create:
 	cd $$GOPATH/src/github/ATTron/utdfgo/utdf && \
-	GOOS=windows go build -o ./release/utdf.exe && \
-	GOOS=linux go build -o ./release/utdf
+	GOOS=windows go build -o ../release/windows/utdf.exe && \
+	GOOS=linux go build -o ../release/linux/utdf && \
+	go build -o ../release/mac/utdf
 
 install-linux-mac:
 	cd $$GOPATH/src/github/ATTron/utdfgo/release && \
--- utdf/utdf.go
+++ utdf/utdf.go
@@ -11,26 +11,37 @@ import (
 )
 
 func main() {
+	// setup cli flags
 	fname := flag.String("filename", "", "file that contains utdf data")
 	rcall := flag.String("run", "ToString", "run specific command")
-	ocall := flag.String("output", "utdfgo", "save output of cli to file")
+	ocall := flag.String("output", "", "save output of cli to file")
 	flag.Parse()
 
 	if *fname == "" {
 		flag.PrintDefaults()
 		os.Exit(0)
 	}
-	f, err := os.Create(*ocall)
-	if err != nil {
-		log.Fatal("Error writing to file:", *ocall)
-	}
-	defer f.Close()
+	// run commands and output to file if requested
 	utdf := utdfgo.Run(*fname)
-	for _, p := range utdf {
+	for i, p := range utdf {
 		cmd := reflect.ValueOf(p).MethodByName(*rcall)
 		output := cmd.Call([]reflect.Value{})
 		fmt.Printf("%v\n", output[0])
-		fmt.Fprintln(f, output[0])
+		if *ocall != "" {
+			if i == 0 {
+				_, err := os.Create(*ocall)
+				if err != nil {
+					log.Fatal("Cannot create file", *ocall)
+				}
+			}
+			f, err := os.OpenFile(*ocall, os.O_APPEND|os.O_WRONLY, 0600)
+			if err != nil {
+				panic(err)
+			}
+			defer f.Close()
+			fmt.Fprintln(f, output[0])
+		}
 	}
-	fmt.Printf("fname: %s, rcall: %s, ocall: %s", *fname, *rcall, *ocall)
+	// useful for debugging
+	// fmt.Printf("fname: %s, rcall: %s, ocall: %s", *fname, *rcall, *ocall)
 }
